247c0999d97e2e69ec18741fad6b56bd69b30d54
sdalexan <30364711+sdalexan@users.noreply.github.com>
2022-02-03 20:20:25 -0700

    SEO data

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Shawn Alexander`,
+    siteTitleAlt: `Shawn Alexander - Resume`,
+    siteHeadline: `Shawn Alexander - Resume`,
+    siteUrl: `https://sdalexander.com`,
+    siteDescription: `Shawn Alexander Resume Information`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@sdalexan`,
   },
   plugins: [
     {
