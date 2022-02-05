3025d64c83cc3c64c3aab7b54a6c4069ca0d2361
sdalexan <30364711+sdalexan@users.noreply.github.com>
2022-02-05 08:33:11 -0700

    added page templates

---
+++ src/pages/javascript.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+  
+class Javascript extends Component {
+  componentDidMount() {
+    // this.initViz()
+  }
+ 
+ 
+  initViz() {
+    // const vizUrl = 'https://public.tableau.com/views/Resume_16439147995410/Shawn?:embed=y&:display_count=yes';
+    // const vizContainer = this.vizContainer;
+    // var options = {
+    //     hideTabs: true,
+    //     onFirstInteractive: function() {
+    //       // The viz is now ready and can be safely used.
+    //     }
+    //  };
+    // let viz = new window.tableau.Viz(vizContainer, vizUrl, options)
+  }
+ 
+ 
+  render() {
+    return (
+<h1>JavaScript</h1>
+    )
+  }
+}
+ 
+ 
+export default Javascript;
